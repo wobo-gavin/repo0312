c3855d57020818a86e20bb309a3a9e77f80e6dd8
Michael Dowling <mtdowling@gmail.com>
2011-04-06 10:58:55 -0500

    Moving vendors to /vendor and using submodules

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,15 +11,15 @@ namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests;
 error_reporting(E_ALL | E_STRICT);
 
 require_once 'PHPUnit/TextUI/TestRunner.php';
-require_once __DIR__ . '/../library/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__ . '/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
     '/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests' => __DIR__,
     '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => __DIR__ . '/../library',
-    'Doctrine' => __DIR__ . '/../library/vendor'
+    'Doctrine' => __DIR__ . '/../vendor/Doctrine/lib'
 ));
 
-$classLoader->registerPrefix('Zend_',  __DIR__ . '/../library/vendor');
+$classLoader->registerPrefix('Zend_',  __DIR__ . '/../vendor');
 
 $classLoader->register();
