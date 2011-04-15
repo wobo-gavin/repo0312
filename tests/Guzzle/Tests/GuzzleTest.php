23bf4d9cf962c732b87a9023ef438ed5b77ed140
Michael Dowling <mtdowling@gmail.com>
2011-04-15 15:38:57 -0500

    [Tests] Add coverage to Guzzle\Guzzle

--- tests/Guzzle/Tests/GuzzleTest.php
+++ tests/Guzzle/Tests/GuzzleTest.php
@@ -19,11 +19,10 @@ class /* Replaced /* Replaced /* Replaced Guzzle */ */ */Test extends /* Replace
      */
     public function testGetDefaultUserAgent()
     {
+        /* Replaced /* Replaced /* Replaced Guzzle */ */ */::reset();
         $version = curl_version();
         $agent = sprintf('/* Replaced /* Replaced /* Replaced Guzzle */ */ *//%s (Language=PHP/%s; curl=%s; Host=%s)', /* Replaced /* Replaced /* Replaced Guzzle */ */ */::VERSION, \PHP_VERSION, $version['version'], $version['host']);
-
         $this->assertEquals($agent, /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getDefaultUserAgent());
-
         // Get it from cache this time
         $this->assertEquals($agent, /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getDefaultUserAgent());
     }
@@ -34,7 +33,6 @@ class /* Replaced /* Replaced /* Replaced Guzzle */ */ */Test extends /* Replace
     public function testGetHttpDate()
     {
         $fmt = 'D, d M Y H:i:s \G\M\T';
-
         $this->assertEquals(gmdate($fmt), /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getHttpDate('now'));
         $this->assertEquals(gmdate($fmt), /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getHttpDate(strtotime('now')));
         $this->assertEquals(gmdate($fmt, strtotime('+1 day')), /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getHttpDate('+1 day'));
@@ -76,10 +74,13 @@ class /* Replaced /* Replaced /* Replaced Guzzle */ */ */Test extends /* Replace
      */
     public function testCachesCurlInfo()
     {
+        /* Replaced /* Replaced /* Replaced Guzzle */ */ */::reset();
         $c = curl_version();
-        $this->assertInternalType('array', /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getCurlInfo());
+        $info = /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getCurlInfo();
+        $this->assertInternalType('array', $info);
         $this->assertEquals(false, /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getCurlInfo('ewfewfewfe'));
         $this->assertEquals($c['version'], /* Replaced /* Replaced /* Replaced Guzzle */ */ */::getCurlInfo('version'));
+        $this->assertSame(/* Replaced /* Replaced /* Replaced Guzzle */ */ */::getCurlInfo(), $info);
     }
 
     /**
