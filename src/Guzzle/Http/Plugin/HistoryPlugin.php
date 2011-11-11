fea9d6ab3b8596f579464a366fc8761c8d207308
Michael Dowling <mtdowling@gmail.com>
2011-11-10 23:04:19 -0600

    [Http] Adding HistoryPlugin.  Closes #8

---
+++ src/Guzzle/Http/Plugin/HistoryPlugin.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin;
+
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Event\Observer;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Event\Subject;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Message\RequestInterface;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Message\Response;
+
+/**
+ * Maintains a list of requests and responses sent using a request or /* Replaced /* Replaced /* Replaced client */ */ */
+ *
+ * @author Michael Dowling <michael@/* Replaced /* Replaced /* Replaced guzzle */ */ */php.org>
+ */
+class HistoryPlugin implements Observer, \IteratorAggregate, \Countable
+{
+    /**
+     * @var int The maximum number of requests to maintain in the history
+     */
+    protected $limit = 10;
+
+    /**
+     * @var array Requests that have passd through the plugin
+     */
+    protected $requests = array();
+
+    /**
+     * Add a request to the history
+     *
+     * @param RequestInterface $request Request to add
+     *
+     * @return HistoryPlugin
+     */
+    public function add(RequestInterface $request)
+    {
+        if ($request->getResponse()) {
+            $this->requests[] = $request;
+            if (count($this->requests) > $this->getlimit()) {
+                array_shift($this->requests);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set the max number of requests to store
+     *
+     * @param int $limit Limit
+     *
+     * @return HistoryPlugin
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = (int) $limit;
+
+        return $this;
+    }
+
+    /**
+     * Get the request limit
+     *
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * Get the requests in the history
+     *
+     * @return ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->requests);
+    }
+
+    /**
+     * Get the number of requests in the history
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->requests);
+    }
+
+    /**
+     * Get the last request sent
+     *
+     * @return RequestInterface
+     */
+    public function getLastRequest()
+    {
+        return end($this->requests);
+    }
+
+    /**
+     * Get the last response in the history
+     *
+     * @return Response
+     */
+    public function getLastResponse()
+    {
+        return $this->getLastRequest()->getResponse();
+    }
+
+    /**
+     * Clears the history
+     *
+     * @return HistoryPlugin
+     */
+    public function clear()
+    {
+        $this->requests = array();
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update(Subject $subject, $event, $context = null)
+    {
+        if ($event == 'request.complete') {
+            $this->add($subject);
+        }
+    }
+}
---
+++ tests/Guzzle/Tests/Http/Plugin/HistoryPluginTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\Http\Plugin;
+
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\/* Replaced /* Replaced /* Replaced Guzzle */ */ */;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Message\RequestFactory;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Message\Request;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Message\Response;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin;
+
+/**
+ * @author Michael Dowling <michael@/* Replaced /* Replaced /* Replaced guzzle */ */ */php.org>
+ */
+class HistoryPluginTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\/* Replaced /* Replaced /* Replaced Guzzle */ */ */TestCase
+{
+    /**
+     * Adds multiple requests to a plugin
+     *
+     * @param HistoryPlugin $h Plugin
+     * @param int $num Number of requests to add
+     *
+     * @return array
+     */
+    protected function addRequests(HistoryPlugin $h, $num)
+    {
+        $requests = array();
+        for ($i = 0; $i < $num; $i++) {
+            $requests[$i] = RequestFactory::get('http://localhost/');
+            $requests[$i]->setResponse(new Response(200), true);
+            $requests[$i]->send();
+            $h->add($requests[$i]);
+        }
+
+        return $requests;
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::getLimit
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::setLimit
+     */
+    public function testMaintainsLimitValue()
+    {
+        $h = new HistoryPlugin();
+        $this->assertSame($h, $h->setLimit(10));
+        $this->assertEquals(10, $h->getLimit());
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::add
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::count
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::getIterator
+     */
+    public function testAddsRequests()
+    {
+        $h = new HistoryPlugin();
+        $requests = $this->addRequests($h, 1);
+        $requests[0]->send();
+        $this->assertEquals(1, count($h));
+        $i = $h->getIterator();
+        $this->assertEquals(1, count($i));
+        $this->assertEquals($requests[0], $i[0]);
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::add
+     */
+    public function testIgnoresUnsentRequests()
+    {
+        $h = new HistoryPlugin();
+        $request = RequestFactory::get('http://localhost/');
+        $h->add($request);
+        $this->assertEquals(0, count($h));
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::add
+     * @depends testAddsRequests
+     */
+    public function testMaintainsLimit()
+    {
+        $h = new HistoryPlugin();
+        $h->setLimit(2);
+        $requests = $this->addRequests($h, 3);
+        $this->assertEquals(2, count($h));
+        $i = 0;
+        foreach ($h as $request) {
+            if ($i > 0) {
+                $this->assertSame($requests[$i], $request);
+            }
+        }
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::getLastRequest
+     */
+    public function testReturnsLastRequest()
+    {
+        $h = new HistoryPlugin();
+        $requests = $this->addRequests($h, 5);
+        $this->assertSame(end($requests), $h->getLastRequest());
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::getLastResponse
+     */
+    public function testReturnsLastResponse()
+    {
+        $h = new HistoryPlugin();
+        $requests = $this->addRequests($h, 5);
+        $this->assertSame(end($requests)->getResponse(), $h->getLastResponse());
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::clear
+     */
+    public function testClearsHistory()
+    {
+        $h = new HistoryPlugin();
+        $requests = $this->addRequests($h, 5);
+        $this->assertEquals(5, count($h));
+        $h->clear();
+        $this->assertEquals(0, count($h));
+    }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\HistoryPlugin::update
+     * @depends testAddsRequests
+     */
+    public function testUpdatesAddRequests()
+    {
+        $h = new HistoryPlugin();
+        $request = RequestFactory::get('http://localhost/');
+        $request->setResponse(new Response(200), true);
+        $request->getEventManager()->attach($h);
+        $request->send();
+        $this->assertSame($request, $h->getLastRequest());
+    }
+}
