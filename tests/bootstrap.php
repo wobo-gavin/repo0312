45f6f4e0205042e18ac55c38c1afec68fea63c4a
Michael Dowling <mtdowling@gmail.com>
2012-01-15 12:31:00 -0600

    Fixing composer.json so that it autoloads from the Guzzle\Tests namespace

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,9 +11,7 @@ if (!file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'composer.lock')) {
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 // Include the composer autoloader
-$loader = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . '.composer' . DIRECTORY_SEPARATOR . 'autoload.php';
-// Register the /* Replaced /* Replaced /* Replaced Guzzle */ */ */ test namespace
-$loader->add('/* Replaced /* Replaced /* Replaced Guzzle */ */ */\\Tests', __DIR__);
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . '.composer' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 // Add the services file to the default service builder
 $servicesFile = __DIR__ . DIRECTORY_SEPARATOR . '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' . DIRECTORY_SEPARATOR . 'Tests' . DIRECTORY_SEPARATOR . 'TestData' . DIRECTORY_SEPARATOR . 'services.xml';
