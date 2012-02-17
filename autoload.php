203b4d2f459586b9af54e5790fe4c64211d76346
Michael Dowling <mtdowling@gmail.com>
2012-02-16 23:12:59 -0600

    Fixing autoloader.  Closes #19

--- autoload.php
+++ autoload.php
@@ -1,24 +1,15 @@
 <?php
 
-$namespaces = array(
-    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => 'phar://' . __FILE__ . '/src',
-    'Symfony\\Component\\Validator' => 'phar://' . __FILE__ . '/vendor/symfony/validator',
-    'Symfony\\Component\\EventDispatcher' => 'phar://' . __FILE__ . '/vendor/symfony/event-dispatcher',
-    'Doctrine' => 'phar://' . __FILE__ . '/vendor/doctrine/common/lib',
-    'Monolog' => 'phar://' . __FILE__ . '/vendor/monolog/monolog/src'
-);
-
-if (DIRECTORY_SEPARATOR == '/') {
-    require_once 'phar://' . __FILE__ . '/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-} else {
-    require_once 'phar://' . __FILE__ . '\\vendor\\symfony\\class-loader\\Symfony\\Component\\ClassLoader\\UniversalClassLoader.php';
-    $namespaces = array_filter($namespaces, function($namespace) {
-        return str_replace('phar:\\\\', 'phar://', str_replace('/', '\\', $namespace));
-    });
-}
+require_once 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$classLoader->registerNamespaces($namespaces);
+$classLoader->registerNamespaces(array(
+    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//src',
+    'Symfony\\Component\\Validator' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/symfony/validator',
+    'Symfony\\Component\\EventDispatcher' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/symfony/event-dispatcher',
+    'Doctrine' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/doctrine/common/lib',
+    'Monolog' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/monolog/monolog/src'
+));
 $classLoader->register();
 
 __HALT_COMPILER();
