afedc630ab3b0c2bd921c64b413c3c6d1d172dec
Julian Higman <jh@kasabi.com>
2012-03-01 11:11:36 +0000

    Added CurlAuthPlugin as a generic plugin for curl auth methods. Removed BasicAuthPlugin and DigestAuthPlugin.

---
+++ tests/Guzzle/Tests/Http/Plugin/CurlAuthPluginTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\Http\Plugin;
+
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\CurlAuthPlugin;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Client;
+
+/**
+ * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Plugin\CurlAuthPlugin
+ */
+class CurlAuthPluginTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\/* Replaced /* Replaced /* Replaced Guzzle */ */ */TestCase
+{
+	public function testAddsBasicAuthentication()
+	{
+		$plugin = new CurlAuthPlugin('michael', 'test');
+		$/* Replaced /* Replaced /* Replaced client */ */ */ = new Client('http://www.test.com/');
+		$/* Replaced /* Replaced /* Replaced client */ */ */->getEventDispatcher()->addSubscriber($plugin);
+		$request = $/* Replaced /* Replaced /* Replaced client */ */ */->get('/');
+		$this->assertEquals('michael', $request->getUsername());
+		$this->assertEquals('test', $request->getPassword());
+	}
+	
+    public function testAddsDigestAuthentication()
+    {
+        $plugin = new CurlAuthPlugin('julian', 'test', CURLAUTH_DIGEST);
+        $/* Replaced /* Replaced /* Replaced client */ */ */ = new Client('http://www.test.com/');
+        $/* Replaced /* Replaced /* Replaced client */ */ */->getEventDispatcher()->addSubscriber($plugin);
+        $request = $/* Replaced /* Replaced /* Replaced client */ */ */->get('/');
+        $this->assertEquals('julian', $request->getUsername());
+        $this->assertEquals('test', $request->getPassword());
+        $this->assertEquals('julian:test', $request->getCurlOptions()->get(CURLOPT_USERPWD));
+        $this->assertEquals(CURLAUTH_DIGEST, $request->getCurlOptions()->get(CURLOPT_HTTPAUTH));
+    }
+}
