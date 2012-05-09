eb5b29dc6f4a9a16478afabe73d8064604b2e0e7
Michael Dowling <mtdowling@gmail.com>
2012-05-09 15:52:04 -0700

    Removing references to Symfony2 validator.  Changing phar alias from Guzzle to guzzle.

--- phar-stub.php
+++ phar-stub.php
@@ -1,14 +1,13 @@
 <?php
 
-require_once 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
-    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//src',
-    'Symfony\\Component\\Validator' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/symfony/validator',
-    'Symfony\\Component\\EventDispatcher' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/symfony/event-dispatcher',
-    'Doctrine' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/doctrine/common/lib',
-    'Monolog' => 'phar:///* Replaced /* Replaced /* Replaced Guzzle */ */ *//vendor/monolog/monolog/src'
+    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//src',
+    'Symfony\\Component\\EventDispatcher' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/symfony/event-dispatcher',
+    'Doctrine' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/doctrine/common/lib',
+    'Monolog' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/monolog/monolog/src'
 ));
 $classLoader->register();
 
