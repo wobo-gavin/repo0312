675c1544df61e7481230a1c41142aef0cddfd9b7
Michael Dowling <mtdowling@gmail.com>
2012-05-09 16:19:47 -0700

    Removing alias from phar file

--- phar-stub.php
+++ phar-stub.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+Phar::mapPhar('/* Replaced /* Replaced /* Replaced guzzle */ */ */.phar');
+
+require_once 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ */.phar/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
-    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//src',
-    'Symfony\\Component\\EventDispatcher' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/symfony/event-dispatcher',
-    'Doctrine' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/doctrine/common/lib',
-    'Monolog' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ *//vendor/monolog/monolog/src'
+    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ */.phar/src',
+    'Symfony\\Component\\EventDispatcher' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ */.phar/vendor/symfony/event-dispatcher',
+    'Doctrine' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ */.phar/vendor/doctrine/common/lib',
+    'Monolog' => 'phar:///* Replaced /* Replaced /* Replaced guzzle */ */ */.phar/vendor/monolog/monolog/src'
 ));
 $classLoader->register();
 
