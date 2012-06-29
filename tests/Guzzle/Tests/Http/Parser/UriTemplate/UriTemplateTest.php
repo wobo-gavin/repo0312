32c433324643428d503b82b00d7d2bc26a1111d9
Michael Dowling <mtdowling@gmail.com>
2012-06-28 18:32:25 -0700

    Allowing for deeply nested URI template composite expansion. Closes #90.

--- tests/Guzzle/Tests/Http/Parser/UriTemplate/UriTemplateTest.php
+++ tests/Guzzle/Tests/Http/Parser/UriTemplate/UriTemplateTest.php
@@ -173,4 +173,30 @@ class UriTemplateTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */ 
         $exp = substr($exp, 1, -1);
         $this->assertEquals($data, $method->invokeArgs($template, array($exp)));
     }
+
+    /**
+     * @ticket https://github.com//* Replaced /* Replaced /* Replaced guzzle */ */ *///* Replaced /* Replaced /* Replaced guzzle */ */ *//issues/90
+     */
+    public function testAllowsNestedArrayExpansion()
+    {
+        $template = new UriTemplate();
+
+        $result = $template->expand('http://example.com{+path}{/segments}{?query,data*,foo*}', array(
+            'path'     => '/foo/bar',
+            'segments' => array('one', 'two'),
+            'query'    => 'test',
+            'data'     => array(
+                'more' => array('fun', 'ice cream')
+            ),
+            'foo' => array(
+                'baz' => array(
+                    'bar'  => 'fizz',
+                    'test' => 'buzz'
+                ),
+                'bam' => 'boo'
+            )
+        ));
+
+        $this->assertEquals('http://example.com/foo/bar/one,two?query=test&more%5B0%5D=fun&more%5B1%5D=ice%20cream&baz%5Bbar%5D=fizz&baz%5Btest%5D=buzz&bam=boo', $result);
+    }
 }
