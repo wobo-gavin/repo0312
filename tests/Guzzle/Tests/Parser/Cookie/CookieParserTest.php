54cab62ccec33d7c26a107658e1d890f98de37e5
Michael Dowling <mtdowling@gmail.com>
2012-07-10 22:22:18 -0700

    [Parser] Not urldecoding cookie values by default. Addresses #99.

--- tests/Guzzle/Tests/Parser/Cookie/CookieParserTest.php
+++ tests/Guzzle/Tests/Parser/Cookie/CookieParserTest.php
@@ -2,10 +2,21 @@
 
 namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\Parser\Cookie;
 
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Parser\Cookie\CookieParser;
+
 /**
  * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Parser\Cookie\CookieParser
  */
 class CookieParserTest extends CookieParserProvider
 {
     protected $cookieParserClass = '/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Parser\Cookie\CookieParser';
+
+    public function testUrlDecodesCookies()
+    {
+        $parser = new CookieParser();
+        $result = $parser->parseCookie('foo=baz+bar', null, null, true);
+        $this->assertEquals(array(
+            'foo' => 'baz bar'
+        ), $result['cookies']);
+    }
 }
