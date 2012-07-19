9f2ded09550a018dd4cdf25bfe913d0ad35d407a
Matt Button <matthew@sigswitch.com>
2012-07-19 13:46:45 +0100

    Treat '+' as encoded space when parsing query string
    According to RFC1738 (implemented by urlencode) spaces are encoded as
    plus symbols. However, RFC3986 (imeplemented by rawurlencode) states
    that spaces should be encoded as '%20'.
    
    When parsing a query string that was encoded with urlencode we should
    treat plus symbols as spaces.
    
    fixes #105

--- tests/Guzzle/Tests/Http/QueryStringTest.php
+++ tests/Guzzle/Tests/Http/QueryStringTest.php
@@ -246,4 +246,13 @@ class QueryStringTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */ 
         $query = QueryString::fromString('?foo=baz&bar=boo');
         $this->assertEquals('?foo=baz&bar=boo', (string) $query);
     }
+
+    /**
+     * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\QueryString::fromString
+     */
+    public function testConvertsPlusSymbolsToSpaces()
+    {
+        $query = QueryString::fromString('var=foo+bar');
+        $this->assertEquals('foo bar', $query->get('var'));
+    }
 }
