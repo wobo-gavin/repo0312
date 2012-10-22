56cae3f50de278d9f3e6f0f6ae89781376edf038
Michael Dowling <mtdowling@gmail.com>
2012-10-22 11:10:37 -0700

    Allowing models to be used more like Collections. Fixing bug where 0 value keys were ignored

--- tests/Guzzle/Tests/Service/Resource/ModelTest.php
+++ tests/Guzzle/Tests/Service/Resource/ModelTest.php
@@ -4,6 +4,7 @@ namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\Service\Reso
 
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Service\Resource\Model;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Service\Description\Parameter;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Collection;
 
 /**
  * @covers /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Service\Resource\Model
@@ -37,5 +38,44 @@ class ModelTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tes
         $this->assertNull($model->getPath('wewewf'));
         $this->assertNull($model->getPath('baz/mesa/jar/jar'));
         $this->assertSame($data, $model->toArray());
+    }
+
+    public function testCanBeUsedWithoutStructure()
+    {
+        $model = new Model(array(
+            'Foo' => 'baz',
+            'Bar' => array(
+                'Boo' => 'Bam'
+            )
+        ));
+        $transform = function ($key, $value) {
+            return ($value && is_array($value)) ? new Collection($value) : $value;
+        };
+        $model = $model->map($transform);
+        $this->assertInstanceOf('/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Collection', $model->getPath('Bar'));
+    }
+
+    public function testFalseyKeysStillDescend()
+    {
+        $model = new Model(array(
+            '0' => array(
+                'a' => 'jar'
+            ),
+            1 => 'other'
+        ));
+        $this->assertEquals('jar', $model->getPath('0/a'));
+        $this->assertEquals('other', $model->getPath('1'));
+    }
+
+    public function testAllowsFiltering()
+    {
+        $model = new Model(array(
+            'Foo' => 'baz',
+            'Bar' => 'a'
+        ));
+        $model = $model->filter(function ($i, $v) {
+            return $v[0] == 'a';
+        });
+        $this->assertEquals(array('Bar' => 'a'), $model->toArray());
     }
 }
