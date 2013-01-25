c9941226c42a4ce789cb84b8f7376532cd4510c1
Ben Parker <ben@mail24.co.uk>
2013-01-25 22:40:55 +0000

    Changes value of CURLOPT_SSL_VERIFYHOST set in Http Client
    1 as a value for CURLOPT_SSL_VERIFYHOST is deprecated
    in libcurl 7.28.1, recommended value is now 2

--- tests/Guzzle/Tests/Http/ClientTest.php
+++ tests/Guzzle/Tests/Http/ClientTest.php
@@ -228,7 +228,7 @@ class ClientTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Te
         $options = $/* Replaced /* Replaced /* Replaced client */ */ */->getConfig('curl.options');
         $this->assertArrayNotHasKey(CURLOPT_CAINFO, $options);
         $this->assertSame(false, $options[CURLOPT_SSL_VERIFYPEER]);
-        $this->assertSame(1, $options[CURLOPT_SSL_VERIFYHOST]);
+        $this->assertSame(2, $options[CURLOPT_SSL_VERIFYHOST]);
     }
 
     /**
