e9b9b2830cf66b3ec099eb500b376e4bb5afb8f9
Michael Dowling <mtdowling@gmail.com>
2013-03-03 13:25:16 -0800

    Adding an ErrorResponse plugin that can throw specific exceptions based on a service description

---
+++ src/Guzzle/Plugin/ErrorResponse/Exception/ErrorResponseException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Plugin\ErrorResponse\Exception;
+
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Exception\RuntimeException;
+
+class ErrorResponseException extends RuntimeException {}
