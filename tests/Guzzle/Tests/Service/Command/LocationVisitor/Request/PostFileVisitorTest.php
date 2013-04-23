39fb92fe11abeebd3bd17fca39e55e7b0ccb6b81
Michael Dowling <mtdowling@gmail.com>
2013-04-23 14:50:50 -0700

    Allowing the setting of multiple POST files for the same key in one call

--- tests/Guzzle/Tests/Service/Command/LocationVisitor/Request/PostFileVisitorTest.php
+++ tests/Guzzle/Tests/Service/Command/LocationVisitor/Request/PostFileVisitorTest.php
@@ -2,6 +2,8 @@
 
 namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Tests\Service\Command\LocationVisitor\Request;
 
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Service\Client;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Service\Description\ServiceDescription;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Http\Message\PostFile;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Service\Command\LocationVisitor\Request\PostFileVisitor as Visitor;
 
@@ -22,5 +24,31 @@ class PostFileVisitorTest extends AbstractVisitorTestCase
         // Test with a PostFile
         $visitor->visit($this->command, $this->request, $param->setSentAs(null), new PostFile('baz', __FILE__));
         $this->assertInternalType('array', $this->request->getPostFile('baz'));
+    }
+
+    public function testVisitsLocationWithMultipleFiles()
+    {
+        $description = ServiceDescription::factory(array(
+            'operations' => array(
+                'DoPost' => array(
+                    'httpMethod' => 'POST',
+                    'parameters' => array(
+                        'foo' => array(
+                            'location' => 'postFile',
+                            'type' => array('string', 'array')
+                        )
+                    )
+                )
+            )
+        ));
+        $this->getServer()->flush();
+        $this->getServer()->enqueue(array("HTTP/1.1 200 OK\r\nContent-Length:0\r\n\r\n"));
+        $/* Replaced /* Replaced /* Replaced client */ */ */ = new Client($this->getServer()->getUrl());
+        $/* Replaced /* Replaced /* Replaced client */ */ */->setDescription($description);
+        $command = $/* Replaced /* Replaced /* Replaced client */ */ */->getCommand('DoPost', array('foo' => array(__FILE__, __FILE__)));
+        $command->execute();
+        $received = $this->getServer()->getReceivedRequests();
+        $this->assertContains('name="foo[0]";', $received[0]);
+        $this->assertContains('name="foo[1]";', $received[0]);
     }
 }
