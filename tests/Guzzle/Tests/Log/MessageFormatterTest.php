40d5d2c5d1d85636a964268d357669a5de66f9e4
Martin Halamicek <martin@halamicek.cz>
2013-05-23 13:49:30 +0200

    Log/MessageFormatter - check if response is passed

--- tests/Guzzle/Tests/Log/MessageFormatterTest.php
+++ tests/Guzzle/Tests/Log/MessageFormatterTest.php
@@ -122,6 +122,12 @@ class MessageFormatterTest extends \/* Replaced /* Replaced /* Replaced Guzzle *
         $this->assertEquals('1/2', $formatter->format($this->request, $response));
     }
 
+	public function testUsesEmptyStringWhenNoHandleAndNoResponse()
+	{
+		$formatter = new MessageFormatter('{connect_time}/{total_time}');
+		$this->assertEquals('/', $formatter->format($this->request));
+	}
+
     public function testInjectsTotalTime()
     {
         $out = '';
