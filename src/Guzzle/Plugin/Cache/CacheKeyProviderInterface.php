41254917ea7a10ad5d281a94b9e41a53e88c9b4c
Michael Dowling <mtdowling@gmail.com>
2013-06-07 10:46:02 -0700

    Adding CacheKeyProviderInterface and DefaultCacheKeyProvider back just in case they are being used on their own but emitting a warning when they are used

---
+++ src/Guzzle/Plugin/Cache/CacheKeyProviderInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Plugin\Cache;
+
+\/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Version::warn('/* Replaced /* Replaced /* Replaced Guzzle */ */ */\Plugin\Cache\CacheKeyProviderInterface is no longer used');
+
+/**
+ * @deprecated This is no longer used
+ * @codeCoverageIgnore
+ */
+interface CacheKeyProviderInterface {}
