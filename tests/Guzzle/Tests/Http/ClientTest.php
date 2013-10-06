0a2639f0ae8a4a33af0d2208a46c6a375edc6108
Michael Dowling <mtdowling@gmail.com>
2013-10-05 18:34:52 -0700

    More coverage for expanding URI template in client with relative path

--- tests/Guzzle/Tests/Http/ClientTest.php
+++ tests/Guzzle/Tests/Http/ClientTest.php
@@ -215,6 +215,15 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    public function testUsesBaseUrlCombinedWithProvidedUrlViaUriTemplate()
+    {
+        $/* Replaced /* Replaced /* Replaced client */ */ */ = new Client(['base_url' => 'http://www.foo.com/baz?bam=bar']);
+        $this->assertEquals(
+            'http://www.foo.com/baz/bar/123',
+            $/* Replaced /* Replaced /* Replaced client */ */ */->createRequest('GET', ['bar/{bam}', ['bam' => '123']])->getUrl()
+        );
+    }
+
     public function testSettingAbsoluteUrlOverridesBaseUrl()
     {
         $/* Replaced /* Replaced /* Replaced client */ */ */ = new Client(['base_url' => 'http://www.foo.com/baz?bam=bar']);
