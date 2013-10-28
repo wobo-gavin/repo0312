2464565382f4386149d28173c10fbae5ee8e2833
Michael Dowling <mtdowling@gmail.com>
2013-10-27 17:29:19 -0700

    Removing ErrorResponsePlugin

--- src/Guzzle/Plugin/ErrorResponse/Exception/ErrorResponseException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Plugin\ErrorResponse\Exception;
-
-use /* Replaced /* Replaced /* Replaced Guzzle */ */ */\Common\Exception\RuntimeException;
-
-class ErrorResponseException extends RuntimeException {}
