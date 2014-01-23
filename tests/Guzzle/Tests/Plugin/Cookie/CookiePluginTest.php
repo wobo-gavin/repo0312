e84a0835938b68b555eef7e3dbc2a19b0723048e
Michael Dowling <mtdowling@gmail.com>
2014-01-23 10:29:07 -0800

    Escaping the regex delimiter when matching Cookie domains. Closes #526.

--- tests/Guzzle/Tests/Plugin/Cookie/CookiePluginTest.php
+++ tests/Guzzle/Tests/Plugin/Cookie/CookiePluginTest.php
@@ -125,4 +125,10 @@ class CookiePluginTest extends \/* Replaced /* Replaced /* Replaced Guzzle */ */
         $plugin = new CookiePlugin($jar);
         $this->assertSame($jar, $plugin->getCookieJar());
     }
+
+    public function testEscapesCookieDomains()
+    {
+        $cookie = new Cookie(array('domain' => '/foo/^$[A-Z]+/'));
+        $this->assertFalse($cookie->matchesDomain('foo'));
+    }
 }
