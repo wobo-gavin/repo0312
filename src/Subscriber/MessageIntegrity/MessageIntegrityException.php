ca42e1490cb12c0df32d611a3471037f49b79770
Michael Dowling <mtdowling@gmail.com>
2014-02-28 15:43:57 -0800

    Moving message integrity plugin to a new repo

--- src/Subscriber/MessageIntegrity/MessageIntegrityException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Subscriber\MessageIntegrity;
-
-use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Exception\RequestException;
-
-class MessageIntegrityException extends RequestException {}
