f3ee5cf75166206a1deaecc01acca79c2dba0737
Jeremy Lindblom <jeremeamia@gmail.com>
2014-04-29 14:12:13 -0700

    Extracted the event/listener management portion of the TransactionIterator into a trait.

--- src/Adapter/TransactionIterator.php
+++ src/Adapter/TransactionIterator.php
@@ -3,6 +3,7 @@
 namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Adapter;
 
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\ClientInterface;
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Event\ListenerAttacherTrait;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Message\RequestInterface;
 
 /**
@@ -11,21 +12,27 @@ use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Message\RequestInter
  */
 class TransactionIterator implements \Iterator
 {
+    use ListenerAttacherTrait;
+
     /** @var \Iterator */
     private $source;
 
     /** @var ClientInterface */
     private $/* Replaced /* Replaced /* Replaced client */ */ */;
 
-    /** @var array of hashes containing 'name', 'fn', 'priority', and 'once' */
-    private $eventListeners;
+    /** @var array Listeners to attach to each request */
+    private $eventListeners = [];
 
     public function __construct(
-        $source, ClientInterface $/* Replaced /* Replaced /* Replaced client */ */ */,
+        $source,
+        ClientInterface $/* Replaced /* Replaced /* Replaced client */ */ */,
         array $options
     ) {
         $this->/* Replaced /* Replaced /* Replaced client */ */ */ = $/* Replaced /* Replaced /* Replaced client */ */ */;
-        $this->configureEvents($options);
+        $this->eventListeners = $this->prepareListeners(
+            $options,
+            ['before', 'complete', 'error']
+        );
         if ($source instanceof \Iterator) {
             $this->source = $source;
         } elseif (is_array($source)) {
@@ -38,21 +45,11 @@ class TransactionIterator implements \Iterator
     public function current()
     {
         $request = $this->source->current();
-
         if (!$request instanceof RequestInterface) {
             throw new \RuntimeException('All must implement RequestInterface');
         }
 
-        if ($this->eventListeners) {
-            $emitter = $request->getEmitter();
-            foreach ($this->eventListeners as $ev) {
-                if ($ev['once']) {
-                    $emitter->once($ev['name'], $ev['fn'], $ev['priority']);
-                } else {
-                    $emitter->on($ev['name'], $ev['fn'], $ev['priority']);
-                }
-            }
-        }
+        $this->attachListeners($request, $this->eventListeners);
 
         return new Transaction($this->/* Replaced /* Replaced /* Replaced client */ */ */, $request);
     }
@@ -73,44 +70,4 @@ class TransactionIterator implements \Iterator
     }
 
     public function rewind() {}
-
-    private function configureEvents(array $options)
-    {
-        static $namedEvents = ['before', 'complete', 'error'];
-
-        foreach ($namedEvents as $event) {
-            if (isset($options[$event])) {
-                if (is_callable($options[$event])) {
-                    $this->eventListeners[] = [
-                        'name'     => $event,
-                        'fn'       => $options[$event],
-                        'priority' => 0,
-                        'once'     => false
-                    ];
-                } else {
-                    $this->addEvent($event, $options[$event]);
-                }
-            }
-        }
-    }
-
-    private function addEvent($eventName, $event)
-    {
-        static $default = ['priority' => 0, 'once' => false];
-
-        if (!is_array($event)) {
-            throw new \InvalidArgumentException('Each event listener must be a'
-                . ' callable or an array of associative arrays where each'
-                . ' associative array contains a "fn" key.');
-        }
-
-        if (isset($event['fn'])) {
-            $event['name'] = $eventName;
-            $this->eventListeners[] = $event + $default;
-        } else {
-            foreach ($event as $e) {
-                $this->addEvent($eventName, $e);
-            }
-        }
-    }
 }
