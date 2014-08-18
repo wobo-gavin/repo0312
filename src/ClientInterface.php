d79755755ac9373414c4d51fa0a45201a160c378
Michael Dowling <mtdowling@gmail.com>
2014-08-17 17:47:21 -0700

    4.2.0 release

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -13,7 +13,7 @@ use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Exception\AdapterExc
  */
 interface ClientInterface extends HasEmitterInterface
 {
-    const VERSION = '4.1.8';
+    const VERSION = '4.2.0';
 
     /**
      * Create and return a new {@see RequestInterface} object.
