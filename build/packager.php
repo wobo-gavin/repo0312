eebeeeb4fc98da4d31130ff632a3c8eac77949f5
Michael Dowling <mtdowling@gmail.com>
2014-08-19 22:02:13 -0700

    Adding automatic github releases

--- build/packager.php
+++ build/packager.php
@@ -1,31 +1,25 @@
 <?php
+require __DIR__ . '/artifacts/Burgomaster.php';
 
-// Copy Burgomaster if it is not present
-$packagerScript = __DIR__ . '/artifacts/Packager.php';
-$packagerSource = 'https://raw.githubusercontent.com/mtdowling/Burgomaster/a4bc5e5600e07436187282fca059755161f8314e/src/Packager.php';
+// Creating staging directory at /* Replaced /* Replaced /* Replaced guzzle */ */ */http/src/build/artifacts/staging.
+$stageDirectory = __DIR__ . '/artifacts/staging';
+// The root of the project is up one directory from the current directory.
+$projectRoot = __DIR__ . '/../';
+$packager = new \Burgomaster($stageDirectory, $projectRoot);
 
-if (!file_exists($packagerScript)) {
-    echo "Retrieving Burgomaster from $packagerSource\n";
-    if (!is_dir(dirname($packagerScript))) {
-        mkdir(dirname($packagerScript)) or die('Unable to create dir');
-    }
-    file_put_contents($packagerScript, file_get_contents($packagerSource));
-    echo "> Downloaded Burgomaster\n\n";
-}
-
-require $packagerScript;
-
-$packager = new \Burgomaster\Packager(
-    realpath(__DIR__ . '/..') . '/build/artifacts/staging',
-    __DIR__ . '/../'
-);
-
+// Copy basic files to the stage directory. Note that we have chdir'd onto
+// the $projectRoot directory, so use relative paths.
 foreach (['README.md', 'LICENSE'] as $file) {
     $packager->deepCopy($file, $file);
 }
 
-$packager->recursiveCopy('src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http');
+// Copy each dependency to the staging directory. Copy *.php and *.pem files.
+$packager->recursiveCopy('src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http', ['php', 'pem']);
 $packager->recursiveCopy('vendor//* Replaced /* Replaced /* Replaced guzzle */ */ */http/streams/src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/Stream');
+// Create the classmap autoloader, and instruct the autoloader to
+// automatically require the '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/functions.php' script.
 $packager->createAutoloader(['/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/functions.php']);
+// Create a phar file from the staging directory at a specific location
 $packager->createPhar(__DIR__ . '/artifacts//* Replaced /* Replaced /* Replaced guzzle */ */ */.phar');
+// Create a zip file from the staging directory at a specific location
 $packager->createZip(__DIR__ . '/artifacts//* Replaced /* Replaced /* Replaced guzzle */ */ */.zip');
