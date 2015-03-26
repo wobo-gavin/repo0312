a2fe0807516b1b0b97759849d8e1b87c320b0486
Michael Dowling <mtdowling@gmail.com>
2015-03-26 10:34:02 -0700

    Unrolling switch

--- tests/Handler/CurlFactoryTest.php
+++ tests/Handler/CurlFactoryTest.php
@@ -86,7 +86,7 @@ class CurlFactoryTest extends \PHPUnit_Framework_TestCase
         Server::enqueue([new /* Replaced /* Replaced /* Replaced Psr7 */ */ */\Response()]);
         $a = new Handler\CurlMultiHandler();
         $req = new /* Replaced /* Replaced /* Replaced Psr7 */ */ */\Request('GET', Server::$url);
-        $a($req, ['curl' => [CURLOPT_LOW_SPEED_LIMIT => 10]]);
+        $a($req, ['curlopts' => [CURLOPT_LOW_SPEED_LIMIT => 10]]);
         $this->assertEquals(10, $_SERVER['_curl'][CURLOPT_LOW_SPEED_LIMIT]);
     }
 
