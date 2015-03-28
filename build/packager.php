f36c5d8a7cee76cab43b0c9af7c5574f405910c5
Michael Dowling <mtdowling@gmail.com>
2015-03-28 13:56:28 -0700

    Fixing packaging

--- build/packager.php
+++ build/packager.php
@@ -13,11 +13,12 @@ foreach (['README.md', 'LICENSE'] as $file) {
 
 // Copy each dependency to the staging directory. Copy *.php and *.pem files.
 $packager->recursiveCopy('src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http', ['php']);
-$packager->recursiveCopy('vendor//* Replaced /* Replaced /* Replaced guzzle */ */ */http/promise/src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/Promise');
+$packager->recursiveCopy('vendor//* Replaced /* Replaced /* Replaced guzzle */ */ */http/promises/src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/Promise');
 $packager->recursiveCopy('vendor//* Replaced /* Replaced /* Replaced guzzle */ */ */http/psr7/src', '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http//* Replaced /* Replaced /* Replaced Psr7 */ */ */');
-$packager->recursiveCopy('vendor/psr/http-message/src', 'Psr\Http\Message');
+$packager->recursiveCopy('vendor/psr/http-message/src', 'Psr/Http/Message');
 
 $packager->createAutoloader([
+    '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/functions.php',
     '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http//* Replaced /* Replaced /* Replaced Psr7 */ */ *//functions.php',
     '/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http/Promise/functions.php',
 ]);
