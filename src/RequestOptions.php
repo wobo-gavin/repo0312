7419df562f1a9f94aa2708fd58b75006a438bd4f
Michael Dowling <mtdowling@gmail.com>
2015-04-25 15:35:33 -0700

    Adding request option constants. Closes #1019

---
+++ src/RequestOptions.php
@@ -0,0 +1,34 @@
+<?php
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http;
+
+/**
+ * This class contains a list of built-in /* Replaced /* Replaced /* Replaced Guzzle */ */ */ request options.
+ *
+ * The documentation for each option can be found at http:///* Replaced /* Replaced /* Replaced guzzle */ */ */php.org/.
+ *
+ * @link http://docs./* Replaced /* Replaced /* Replaced guzzle */ */ */php.org/en/v6/request-options.html
+ */
+final class RequestOptions
+{
+    const ALLOW_REDIRECTS = 'allow_redirects';
+    const AUTH = 'auth';
+    const BODY = 'body';
+    const CERT = 'cert';
+    const COOKIES = 'cookies';
+    const CONNECT_TIMEOUT = 'connect_timeout';
+    const DEBUG = 'debug';
+    const DECODE_CONTENT = 'decode_content';
+    const DELAY = 'delay';
+    const EXPECT = 'expect';
+    const HEADERS = 'headers';
+    const HTTP_ERRORS = 'http_errors';
+    const JSON = 'json';
+    const PROXY = 'proxy';
+    const QUERY = 'query';
+    const SINK = 'sink';
+    const SSL_KEY = 'ssl_key';
+    const STREAM = 'stream';
+    const VERIFY = 'verify';
+    const TIMEOUT = 'timeout';
+    const VERSION = 'version';
+}
