ed781345813dd4ea82eea6591e2cb7d0990f2219
Michael Dowling <mtdowling@gmail.com>
2015-04-25 15:03:28 -0700

    Adding handler context to request exceptions

--- tests/Handler/CurlHandlerTest.php
+++ tests/Handler/CurlHandlerTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Test\Handler;
 
+use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Exception\ConnectException;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Handler\CurlHandler;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\/* Replaced /* Replaced /* Replaced Psr7 */ */ */\Request;
 use /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\/* Replaced /* Replaced /* Replaced Psr7 */ */ */\Response;
@@ -77,5 +78,19 @@ class CurlHandlerTest extends \PHPUnit_Framework_TestCase
         $s = microtime(true);
         $a($request, ['delay' => 0.1])->wait();
         $this->assertGreaterThan(0.0001, microtime(true) - $s);
+    }
+
+    public function testCreatesCurlErrorsWithContext()
+    {
+        $handler = new CurlHandler();
+        $request = new Request('GET', 'http://localhost:123');
+        $called = false;
+        $p = $handler($request, ['timeout' => 0.001, 'connect_timeout' => 0.001])
+            ->otherwise(function (ConnectException $e) use (&$called) {
+                $called = true;
+                $this->assertArrayHasKey('errno', $e->getHandlerContext());
+            });
+        $p->wait();
+        $this->assertTrue($called);
     }
 }
