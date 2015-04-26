02dbde3abccd8941e114e1e39db67a5a8e7fd1dc
Michael Dowling <mtdowling@gmail.com>
2015-04-26 12:40:53 -0700

    Cleaning up curl integration

---
+++ src/Handler/EasyHandle.php
@@ -0,0 +1,14 @@
+<?php
+namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Handler;
+
+/**
+ * Represents a cURL easy handle and the data it populates.
+ *
+ * @internal The API of this class may change.
+ */
+final class EasyHandle
+{
+    public $handle;
+    public $body;
+    public $headers = [];
+}
