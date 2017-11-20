11351687e37b498403e11e05755a962a029bf08e
Jonas Stendahl <jonas.stendahl@gmail.com>
2017-11-20 15:38:19 +0100

    Add annotations for exception methods

--- src/Exception/GuzzleException.php
+++ src/Exception/GuzzleException.php
@@ -1,4 +1,13 @@
 <?php
 namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Exception;
 
+/**
+ * @method string getMessage()
+ * @method \Throwable|null getPrevious()
+ * @method mixed getCode()
+ * @method string getFile()
+ * @method int getLine()
+ * @method array getTrace()
+ * @method string getTraceAsString()
+ */
 interface /* Replaced /* Replaced /* Replaced Guzzle */ */ */Exception {}
