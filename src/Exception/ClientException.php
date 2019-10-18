9971e96f7275ddbf15436efc71b0cbc7a054b037
Nyholm <tobias.nyholm@gmail.com>
2019-10-18 12:36:53 +0200

    Added workflows for static analysis

--- src/Exception/ClientException.php
+++ src/Exception/ClientException.php
@@ -4,4 +4,6 @@ namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Exception;
 /**
  * Exception when a /* Replaced /* Replaced /* Replaced client */ */ */ error is encountered (4xx codes)
  */
-class ClientException extends BadResponseException {}
+class ClientException extends BadResponseException
+{
+}
