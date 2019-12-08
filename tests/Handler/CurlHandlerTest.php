af3da6b021faea99eec6da7d2c62d63158b658ad
Mponos George <gmponos@gmail.com>
2019-12-08 17:51:18 +0200

    Remove deprecation of phpunit for assertContains (#2436)
    * Remove deprecation of phpunit for assertContains
    
    * Revert
    
    * Revert

--- tests/Handler/CurlHandlerTest.php
+++ tests/Handler/CurlHandlerTest.php
@@ -36,8 +36,8 @@ class CurlHandlerTest extends TestCase
         Server::enqueue([$response, $response]);
         $a = new CurlHandler();
         $request = new Request('GET', Server::$url);
-        self::assertInstanceOf('/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Promise\FulfilledPromise', $a($request, []));
-        self::assertInstanceOf('/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Promise\FulfilledPromise', $a($request, []));
+        self::assertInstanceOf(\/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Promise\FulfilledPromise::class, $a($request, []));
+        self::assertInstanceOf(\/* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Promise\FulfilledPromise::class, $a($request, []));
     }
 
     public function testDoesSleep()
