c67fb302a9aa1e6ae9ef96524b04f6f92bd6e0ae
Mponos George <gmponos@gmail.com>
2019-12-08 17:01:30 +0200

    Remove check about throwable if it exists (#2426)

--- src/Exception/GuzzleException.php
+++ src/Exception/GuzzleException.php
@@ -3,21 +3,6 @@ namespace /* Replaced /* Replaced /* Replaced Guzzle */ */ */Http\Exception;
 
 use Throwable;
 
-if (interface_exists(Throwable::class)) {
-    interface /* Replaced /* Replaced /* Replaced Guzzle */ */ */Exception extends Throwable
-    {
-    }
-} else {
-    /**
-     * @method string getMessage()
-     * @method \Throwable|null getPrevious()
-     * @method mixed getCode()
-     * @method string getFile()
-     * @method int getLine()
-     * @method array getTrace()
-     * @method string getTraceAsString()
-     */
-    interface /* Replaced /* Replaced /* Replaced Guzzle */ */ */Exception
-    {
-    }
+interface /* Replaced /* Replaced /* Replaced Guzzle */ */ */Exception extends Throwable
+{
 }
