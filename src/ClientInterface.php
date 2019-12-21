823ccd0a9bb17a52af7287f6de29304884c3822a
Tobias Nyholm <tobias.nyholm@gmail.com>
2019-12-21 09:51:15 +0100

    Prepare CHANGELOG for 6.5.1 (#2481)

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -15,7 +15,7 @@ interface ClientInterface
     /**
      * @deprecated Will be removed in /* Replaced /* Replaced /* Replaced Guzzle */ */ */ 7.0.0
      */
-    const VERSION = '6.5.0';
+    const VERSION = '6.5.1';
 
     /**
      * Send an HTTP request.
